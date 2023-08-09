f4f2b4501f9e365ca463978240b487dbaeaeb89a
Tustoz <maxiadityaksm@gmail.com>
2023-08-09 13:34:09 +0700

    a

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 
-app.use(cors());
-app.use(express.json());
+app.use(express.json())
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
